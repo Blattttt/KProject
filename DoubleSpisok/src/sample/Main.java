312facbe5b8c00113914f84e04159ad0dfddab33
Blattttt <77572968+Blattttt@users.noreply.github.com>
2022-04-09 17:14:23 +0300

    Update Main.java

--- DoubleSpisok/src/sample/Main.java
+++ DoubleSpisok/src/sample/Main.java
@@ -12,8 +12,34 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
     public static void main(String[] args) {
-        launch(args);
+            launch(args);
     }
+
+    Group root = new Group();
+    VBox strings = new VBox();
+
+    HBox lineOne = new HBox();
+    TextField boxOne = new TextField();
+    Button btnOneDown = new Button("down");
+
+    HBox lineTwo = new HBox();
+    TextField boxTwo = new TextField();
+    Button btnTwoUp = new Button("up");
+    Button btnTwoDown = new Button("down");
+
+    HBox lineThree = new HBox();
+    TextField boxThree = new TextField();
+    Button btnThreeUp = new Button("up");
+    Button btnThreeDown = new Button("down");
+
+    HBox lineFour = new HBox();
+    TextField boxFour = new TextField();
+    Button btnFourUp = new Button("up");
+    Button btnFourDown = new Button("down");
+
+    HBox lineFive = new HBox();
+    TextField boxFive = new TextField();
+    Button btnFiveUp = new Button("up");
 
     @Override
     public void start(Stage primaryStage) {
@@ -29,128 +55,143 @@ public class Main extends Application{
         strings.getChildren().add(lineFive);
 
         lineOne.setSpacing(5);
-        lineOne.getChildren().add(TextOne);
         lineOne.getChildren().add(boxOne);
         lineOne.getChildren().add(btnOneDown);
 
         lineTwo.setSpacing(5);
-        lineTwo.getChildren().add(TextTwo);
         lineTwo.getChildren().add(boxTwo);
         lineTwo.getChildren().add(btnTwoUp);
         lineTwo.getChildren().add(btnTwoDown);
 
         lineThree.setSpacing(5);
-        lineThree.getChildren().add(TextThree);
         lineThree.getChildren().add(boxThree);
         lineThree.getChildren().add(btnThreeUp);
         lineThree.getChildren().add(btnThreeDown);
 
         lineFour.setSpacing(5);
-        lineFour.getChildren().add(TextFour);
         lineFour.getChildren().add(boxFour);
         lineFour.getChildren().add(btnFourUp);
         lineFour.getChildren().add(btnFourDown);
 
         lineFive.setSpacing(5);
-        lineFive.getChildren().add(TextFive);
         lineFive.getChildren().add(boxFive);
         lineFive.getChildren().add(btnFiveUp);
 
         btnOneDown.setOnAction(e -> {
             if (!boxTwo.getText().equals("") & !boxOne.getText().equals("")){
-                text.setText(boxOne.getText());
+                try{text.setText(boxOne.getText());
                 boxOne.setText(boxTwo.getText());
-                boxTwo.setText(text.getText());}
+                boxTwo.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxThree.getText().equals("") & !boxOne.getText().equals("")){
-                text.setText(boxOne.getText());
+                try{text.setText(boxOne.getText());
                 boxOne.setText(boxThree.getText());
-                boxThree.setText(text.getText());}
+                boxThree.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxFour.getText().equals("") & !boxOne.getText().equals("")){
-                text.setText(boxOne.getText());
+                try{text.setText(boxOne.getText());
                 boxOne.setText(boxFour.getText());
-                boxFour.setText(text.getText());}
+                boxFour.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxFive.getText().equals("") & !boxOne.getText().equals("")){
-                text.setText(boxOne.getText());
+                try{text.setText(boxOne.getText());
                 boxOne.setText(boxFive.getText());
-                boxFive.setText(text.getText());}});
+                boxFive.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}});
 
         btnTwoUp.setOnAction(e -> {
             if (!boxTwo.getText().equals("") & !boxOne.getText().equals("")){
-                text.setText(boxTwo.getText());
+                try{text.setText(boxTwo.getText());
                 boxTwo.setText(boxOne.getText());
-                boxOne.setText(text.getText());}});
+                boxOne.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}});
 
         btnTwoDown.setOnAction(e -> {
             if (!boxThree.getText().equals("") & !boxTwo.getText().equals("")){
-                text.setText(boxTwo.getText());
+                try{text.setText(boxTwo.getText());
                 boxTwo.setText(boxThree.getText());
-                boxThree.setText(text.getText());}
+                boxThree.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxFour.getText().equals("") & !boxTwo.getText().equals("")){
-                text.setText(boxTwo.getText());
+                try{text.setText(boxTwo.getText());
                 boxTwo.setText(boxFour.getText());
-                boxFour.setText(text.getText());}
+                boxFour.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxFive.getText().equals("") & !boxTwo.getText().equals("")){
-                text.setText(boxTwo.getText());
+                try{text.setText(boxTwo.getText());
                 boxTwo.setText(boxFive.getText());
-                boxFive.setText(text.getText());}});
+                boxFive.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}});
 
         btnThreeUp.setOnAction(e -> {
             if (!boxThree.getText().equals("") & !boxTwo.getText().equals("")){
-                text.setText(boxThree.getText());
+                try{text.setText(boxThree.getText());
                 boxThree.setText(boxTwo.getText());
-                boxTwo.setText(text.getText());}
+                boxTwo.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxOne.getText().equals("") & !boxThree.getText().equals("")){
-                text.setText(boxThree.getText());
+                try{text.setText(boxThree.getText());
                 boxThree.setText(boxOne.getText());
-                boxOne.setText(text.getText());}});
+                boxOne.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}});
 
         btnThreeDown.setOnAction(e -> {
             if (!boxFour.getText().equals("") & !boxThree.getText().equals("")){
-                text.setText(boxThree.getText());
+                try{text.setText(boxThree.getText());
                 boxThree.setText(boxFour.getText());
-                boxFour.setText(text.getText());}
+                boxFour.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxFive.getText().equals("") & !boxThree.getText().equals("")){
-                text.setText(boxThree.getText());
+                try{text.setText(boxThree.getText());
                 boxThree.setText(boxFive.getText());
-                boxFive.setText(text.getText());}});
+                boxFive.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}});
 
         btnFourUp.setOnAction(e -> {
             if (!boxFour.getText().equals("") & !boxThree.getText().equals("")){
-                text.setText(boxFour.getText());
+                try{text.setText(boxFour.getText());
                 boxFour.setText(boxThree.getText());
-                boxThree.setText(text.getText());}
+                boxThree.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxTwo.getText().equals("") & !boxFour.getText().equals("")){
-                text.setText(boxFour.getText());
+                try{text.setText(boxFour.getText());
                 boxFour.setText(boxTwo.getText());
-                boxTwo.setText(text.getText());}
+                boxTwo.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxOne.getText().equals("") & !boxFour.getText().equals("")){
-                text.setText(boxFour.getText());
+                try{text.setText(boxFour.getText());
                 boxFour.setText(boxOne.getText());
-                boxOne.setText(text.getText());}});
+                boxOne.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}});
 
         btnFourDown.setOnAction(e -> {
             if (!boxFive.getText().equals("") & !boxFour.getText().equals("")){
-                text.setText(boxFour.getText());
+                try{text.setText(boxFour.getText());
                 boxFour.setText(boxFive.getText());
-                boxFive.setText(text.getText());}});
+                boxFive.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}});
 
         btnFiveUp.setOnAction(e -> {
             if (!boxFive.getText().equals("") & !boxFour.getText().equals("")){
-                text.setText(boxFive.getText());
+                try{text.setText(boxFive.getText());
                 boxFive.setText(boxFour.getText());
-                boxFour.setText(text.getText());}
+                boxFour.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxThree.getText().equals("") & !boxFive.getText().equals("")){
-                text.setText(boxFive.getText());
+                try{text.setText(boxFive.getText());
                 boxFive.setText(boxThree.getText());
-                boxThree.setText(text.getText());}
+                boxThree.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxTwo.getText().equals("") & !boxFive.getText().equals("")){
-                text.setText(boxFive.getText());
+                try{text.setText(boxFive.getText());
                 boxFive.setText(boxTwo.getText());
-                boxTwo.setText(text.getText());}
+                boxTwo.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}
             else if(!boxOne.getText().equals("") & !boxFive.getText().equals("")){
-                text.setText(boxFive.getText());
+                try{text.setText(boxFive.getText());
                 boxFive.setText(boxOne.getText());
-                boxOne.setText(text.getText());}});
+                boxOne.setText(text.getText());
+                }catch(Exception t){boxOne.setText(t + " ");}}});
 
         boxTwo.setEditable(false);
         boxThree.setEditable(false);
@@ -159,27 +200,22 @@ public class Main extends Application{
 
         boxOne.textProperty().addListener((observableValue, s, t1)-> {
             boxTwo.setEditable(true);
-            if(boxOne.getLength() > 20){
-                TextOne.setText("err");} else {TextOne.setText("    ");}});
+            if(t1.length() == 21){boxOne.setText(s);}});
 
         boxTwo.textProperty().addListener((observableValue, s, t1)-> {
             boxThree.setEditable(true);
-            if(boxTwo.getLength() > 20){
-                TextTwo.setText("err");} else {TextTwo.setText("    ");}});
+            if(t1.length() == 21){boxTwo.setText(s);}});
 
         boxThree.textProperty().addListener((observableValue, s, t1)-> {
             boxFour.setEditable(true);
-            if(boxThree.getLength() > 20){
-                TextThree.setText("err");} else {TextThree.setText("    ");}});
+            if(t1.length() == 21){boxThree.setText(s);}});
 
         boxFour.textProperty().addListener((observableValue, s, t1)-> {
             boxFive.setEditable(true);
-            if(boxFour.getLength() > 20){
-                TextFour.setText("err");} else {TextFour.setText("    ");}});
+            if(t1.length() == 21){boxFour.setText(s);}});
 
         boxFive.textProperty().addListener((observableValue, s, t1)-> {
-            if(boxFive.getLength() > 20){
-                TextFive.setText("err");} else {TextFive.setText("    ");}});
+            if(t1.length() == 21){boxFive.setText(s);}});
 
         int WIDTH = 300;
         int HEIGHT = 250;
@@ -188,35 +224,4 @@ public class Main extends Application{
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    Group root = new Group();
-    VBox strings = new VBox();
-
-    HBox lineOne = new HBox();
-    Text TextOne = new Text("    ");
-    TextField boxOne = new TextField();
-    Button btnOneDown = new Button("down");
-
-    HBox lineTwo = new HBox();
-    Text TextTwo = new Text("    ");
-    TextField boxTwo = new TextField();
-    Button btnTwoUp = new Button("up");
-    Button btnTwoDown = new Button("down");
-
-    HBox lineThree = new HBox();
-    Text TextThree = new Text("    ");
-    TextField boxThree = new TextField();
-    Button btnThreeUp = new Button("up");
-    Button btnThreeDown = new Button("down");
-
-    HBox lineFour = new HBox();
-    Text TextFour = new Text("    ");
-    TextField boxFour = new TextField();
-    Button btnFourUp = new Button("up");
-    Button btnFourDown = new Button("down");
-
-    HBox lineFive = new HBox();
-    Text TextFive = new Text("    ");
-    TextField boxFive = new TextField();
-    Button btnFiveUp = new Button("up");
 }
